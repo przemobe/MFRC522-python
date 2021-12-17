6d5ad944c194f971e612061c4a689151e1ab2408
przemobe <przemobe@users.noreply.github.com>
2021-12-17 22:46:03 +0100

    Add example files, update README.md

--- example1.py
+++ example1.py
@@ -1,30 +1,3 @@
-# fork-reason
-
-The goal is to extend the library to support EMV card reading.
-
-1) Add EMV Contactless Communication Protocol functions
-2) Add support for double and triple size UID
-3) Port to MicroPython v1.17 (RP2) (see micropython branch)
-
-# mfrc522
-
-A python library to read/write RFID tags via the budget MFRC522 RFID module.
-
-This code was published in relation to a [blog post](https://pimylifeup.com/raspberry-pi-rfid-rc522/) and you can find out more about how to hook up your MFRC reader to a Raspberry Pi there.
-
-## Installation
-
-Until the package is on PyPi, clone this repository and run `python setup.py install` in the top level directory.
-
-# mfrc522/EmvComMFRC522
-
-A python library implements EMV Contactless Communication Protocol.
-
-## Example Code
-
-The following code will read EMV card and print Select PSE Response: 
-
-```python
 #!/usr/bin/python3
 
 import RPi.GPIO as GPIO
@@ -38,6 +11,32 @@ def getApduCmdSelectByName(name):
     buf.append(len(name))
     buf.extend(ord(c) for c in name)
     buf.append(0x00)
+    return buf
+
+
+def getApduCmdSelectByAid(aid):
+    buf = [0x00, 0xA4, 0x04, 0x00]
+    buf.append(len(aid))
+    buf.extend(aid)
+    buf.append(0x00)
+    return buf
+
+
+def getApduCmdGetProcessingOptions(data=[0x83, 0x00]):
+    # EMV Book 3, 6.5.8
+    buf = [0x80, 0xA8, 0x00, 0x00]
+    buf.append(len(data))
+    buf.extend(data)
+    buf.append(0x00)
+    return buf
+
+
+def getApduCmdReadRecord(sfi, recordNum):
+    # EMV Book 3, 6.5.11
+    buf = [0x00, 0xB2]
+    buf.append(recordNum) # P1
+    buf.append((sfi << 3) | 0x04) # P2
+    buf.append(0x00) # LE
     return buf
 
 
@@ -80,21 +79,3 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         GPIO.cleanup()
         raise
-```
-
-Example output for a card:
-
-```
-Hold a card near the reader
-UID:  EF:F6:A5:B2:0E
-SAK[0]: 0x28
-ATS:  13:78:80:82:02:80:31:80:66:B0:84:12:01:6E:01:83:00:90:00:DD:C7
-Select PSE Response:  6F:2B:84:0E:32:50:41:59:2E:53:59:53:2E:44:44:46:30:31:A5:19:BF:0C:16:61:14:4F:07:A0:00:00:00:03:20:10:50:09:56:49:53:41:20:43:41:52:44:90:00
-=============================================
-```
-
-The response can be decoded using [TLV decoder](https://emvlab.org/tlvutils/):
-
-![obraz](https://user-images.githubusercontent.com/11823937/144728885-9c4fb121-578a-4b29-89df-3ad6a92c25fe.png)
-
-
